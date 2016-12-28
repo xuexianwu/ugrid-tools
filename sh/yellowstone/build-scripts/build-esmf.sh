0520c146d8554480388dbc392f9076792ef5b41f
bekozi <benkoziol@gmail.com>
2016-12-28 16:19:20 -0700

    ENH: Generate regridding weights for 250m CONUS dataset #50
    Working Yellowstone scripts for generating 250m gridded weights with
    NHD Catchments.

--- sh/yellowstone/build-scripts/build-esmf.sh
+++ sh/yellowstone/build-scripts/build-esmf.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-export PREFIX=/glade/u/home/benkoz/sandbox/esmf_HEAD
 export OUTDIR=/glade/u/home/benkoz/logs/esmf-build
-export SRCDIR=~/src/esmf
+export SRCDIR_NAME=esmf_dev
 export BUILDDIR=`mktemp -d`
 export CPU_COUNT=1
-export ESMF_DIR=/glade/u/home/benkoz/src/esmf
+export SHOULD_GIT_CLONE="false"
+export SHOULD_GIT_PULL="false"
 
-#cd ~/src && \
-#git clone git://git.code.sf.net/p/esmf/esmf
+export PREFIX=/glade/u/home/benkoz/sandbox/${SRCDIR_NAME}
+export ESMF_DIR=${BUILDDIR}/${SRCDIR_NAME}
+export SRCDIR=~/src/${SRCDIR_NAME}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+if [ ${SHOULD_GIT_CLONE} == "true" ]; then
+    cd ~/src && \
+     git clone git://git.code.sf.net/p/esmf/esmf
+fi
+
+if [ ${SHOULD_GIT_PULL} == "true" ]; then
+    ( cd ~/src/esmf && git pull )
+fi
+
 cp -r ${SRCDIR} ${BUILDDIR}
-cd ${BUILDDIR} && \
-# git checkout master && \
-# git pull
+cd ${ESMF_DIR}
 
-rm -r ${PREFIX}
+rm -rf ${PREFIX}
 
-#module swap intel gnu
+module swap intel gnu
 
 export ESMF_INSTALL_PREFIX=${PREFIX}
 export ESMF_INSTALL_BINDIR=${PREFIX}/bin
@@ -29,9 +40,11 @@ export ESMF_COMM=mpich2
 #export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 #export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
 
-make clean && \
- make info 2>&1 | tee "${OUTDIR}/esmf.make.info.`date`.out" && \
+#make clean && \
+make info 2>&1 | tee "${OUTDIR}/esmf.make.info.`date`.out" && \
  make -j ${CPU_COUNT} 2>&1 | tee "${OUTDIR}/esmf.make.`date`.out" && \
 # make check
 # make all_tests | tee ~/esmf_all_tests.out
  make install 2>&1 | tee "${OUTDIR}/esmf.make.install.`date`.out"
+
+rm -rf ${BUILDDIR}
--- src/utools/prep/create_netcdf_data.py
+++ src/utools/prep/create_netcdf_data.py
@@ -1,19 +1,23 @@
+import itertools
+
 import netCDF4 as nc
 import numpy as np
 
+from utools.helpers import get_datetime_fp_string
 from utools.io.helpers import get_bounds_from_1d, get_exact_field
 
 
 def create_high_resolution_ucar_grid():
-    path = '/tmp/high_resolution_uccar_exact_data_20160811.nc'
-    lon = np.linspace(-133.50735, -60.492672, num=4608)
-    lat = np.linspace(20.077797, 57.772186, num=3840)
+    path = '/tmp/high_resolution_ucar_exact_data_500m_{}.nc'.format(get_datetime_fp_string())
+    lon = np.linspace(-133.50735, -60.492672, num=4608 * 2)
+    lat = np.linspace(20.077797, 57.772186, num=3840 * 2)
     ttime = [10.]
-    create_source_netcdf_data(path, lon, lat, ttime)
+    create_source_netcdf_data(path, lon, lat, ttime, create_data_variable=False)
 
 
-def create_source_netcdf_data(path, lon, lat, ttime, variable_name='exact'):
-    ds = nc.Dataset(path, 'w', format='NETCDF3_CLASSIC')
+def create_source_netcdf_data(path, lon, lat, ttime, variable_name='exact', create_data_variable=True):
+    ds = nc.Dataset(path, 'w')
+    # ds = nc.Dataset(path, 'w', format='NETCDF3_CLASSIC')
     try:
         ds.createDimension('lon', size=len(lon))
         ds.createDimension('lat', size=len(lat))
@@ -49,12 +53,14 @@ def create_source_netcdf_data(path, lon, lat, ttime, variable_name='exact'):
         vtime.units = 'days since 2000-1-1'
         vtime.calendar = 'standard'
 
-        mlon, mlat = np.meshgrid(lon, lat)
-        exact = get_exact_field(mlat, mlon)
-        fill_exact = np.zeros((len(ttime), lat.shape[0], lon.shape[0]), dtype=np.float32)
-        fill_exact[:] = exact
-        vexact = ds.createVariable(variable_name, np.float32, dimensions=('time', 'lat', 'lon'))
-        vexact[:] = fill_exact
+        if create_data_variable:
+            mlon, mlat = np.meshgrid(lon, lat)
+            exact_shape = (len(ttime), lat.shape[0], lon.shape[0])
+            vexact = ds.createVariable(variable_name, np.float32, dimensions=('time', 'lat', 'lon'))
+            # Use this fill fill approach to avoid memory issues with large grids.
+            for tidx, lon_idx in itertools.product(*[range(ii) for ii in [exact_shape[0], exact_shape[1]]]):
+                exact = get_exact_field(mlon[:, lon_idx], mlat[:, lon_idx])
+                vexact[tidx, :, lon_idx] = exact
     finally:
         ds.close()
 
